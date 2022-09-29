ea3804e8d0197457a3ecbcc794e348a837fec3d9
mnaibei <muchajulius@gmail.com>
2022-09-29 11:50:17 +0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Mucha Naibei on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
